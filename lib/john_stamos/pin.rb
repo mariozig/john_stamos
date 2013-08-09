0445f9cdda9939d42c135d4ed726e6159215330d
mariozig <mariozig@gmail.com>
2013-08-08 20:12:37 -0700

    Update the SearchScraper and Pin classes to emphasize retrieving data via embedded json when possible instead of scraping the HTML.  This was a result of Pinterest making a lot of changes today and tests with old cassettes actually being broken.
    Everything is updated/fixed now.

--- lib/john_stamos/pin.rb
+++ lib/john_stamos/pin.rb
@@ -9,40 +9,71 @@ class JohnStamos::Pin
   end
 
   def image
-    sized_image_url_parts = page.xpath('//meta[@name="og:image"]/@content').first.value.split('/')
-    sized_image_url_parts[3] = "originals"
-    sized_image_url_parts.join('/')
+    embedded_pin_data["images"]["orig"]["url"]
+  end
+
+  def video?
+    embedded_pin_data("is_video")
   end
 
   def description
-    page.xpath('//meta[@name="og:description"]/@content').first.value
+    embedded_pin_data("description")
   end
 
-  def board_title
-    page.xpath('//meta[@name="og:title"]/@content').first.value
+  # TODO: Make this a Board object... not just a url
+  def board
+    embedded_pin_data["board"]["url"]
   end
 
   def like_count
-    page.xpath('//meta[@name="pinterestapp:likes"]/@content').first.value.to_i
+    embedded_pin_data("like_count")
   end
 
   def repin_count
-    page.xpath('//meta[@name="pinterestapp:repins"]/@content').first.value.to_i
+    embedded_pin_data("repin_count")
   end
 
   def source_url
-    page.xpath('//meta[@name="pinterestapp:source"]/@content').first.value
+    embedded_pin_data("link")
   end
 
   def pinner
+    JohnStamos::Pinner.new(embedded_pin_data["pinner"]["username"])
   end
 
   def url
     "http://pinterest.com/pin/#{@id}/"
   end
 
-  def page
-    @page ||= Nokogiri::HTML(open(url))
-  end
+
+
+  private
+    def page
+      @page ||= Nokogiri::HTML(open(url))
+    end
+
+    def embedded_pin_json
+      embedded_script = page.css('script').select do |script|
+        script['src'].nil? && script.content.include?('P.start(')
+      end
+      embedded_script_content = embedded_script.first.content
+      # This regex used in the range below looks for Pinterest's call to `P.start`
+      # and snatches it's parameter... which happens to be a JSON representation of
+      # the page.
+      raw_json = embedded_script_content[/P.start\((.*)\);$/, 1]
+      embedded_script_json = JSON.parse(raw_json)
+
+      embedded_script_json
+    end
+
+    def embedded_pin_data(attribute=nil)
+      value = embedded_pin_json["tree"]["options"]["module"]["data"]
+
+      if !attribute.nil?
+        value = value[attribute]
+      end
+
+      value
+    end
 
 end
--- spec/lib/john_stamos/pin_spec.rb
+++ spec/lib/john_stamos/pin_spec.rb
@@ -7,27 +7,35 @@ describe JohnStamos::Pin do
   end
 
   it { pin.should respond_to(:id) }
+  it { pin.id.should eq(412149803369441273) }
   it { pin.should respond_to(:image) }
   it { pin.should respond_to(:description) }
   it { pin.should respond_to(:pinner) }
-  it { pin.should respond_to(:board_title) }
+  it { pin.should respond_to(:board) }
   it { pin.should respond_to(:like_count) }
   it { pin.should respond_to(:repin_count) }
   it { pin.should respond_to(:source_url) }
   it { pin.should respond_to(:url) }
-
+  it { pin.should respond_to(:video?) }
   it { pin.url.should eq('http://pinterest.com/pin/412149803369441273/') }
 
   describe 'scraped data', :vcr do
     it { pin.image.should eq('http://media-cache-ak0.pinimg.com/originals/ee/39/6e/ee396ebad5cffd3f8f6e83b5fd9f07cb.jpg') }
     it { pin.description.should eq('JohnStamosgemPin description') }
-    it { pin.board_title.should eq('JohnStamosgemBoard') }
+    it { pin.board.should eq('/johnstamosgem/johnstamosgemboard/') }
     it { pin.like_count.should eq(1) }
     it { pin.repin_count.should eq(0) }
     it { pin.source_url.should eq('http://johnstamosgem.com/') }
+    it { pin.video?.should be_false }
 
     describe '#pinner' do
-      it 'returns a Pinner object'
+      it 'returns a Pinner object' do
+        pin.pinner.should be_a(JohnStamos::Pinner)
+      end
+
+      it 'shows my fake account as the owning Pinner' do
+        pin.pinner.username.should eq('johnstamosgem')
+      end
     end
   end
 
