93354928fcea14b489df94cec067901c1a04c9de
mariozig <mariozig@gmail.com>
2013-08-09 17:31:16 -0700

    Code style ::sunglasses::

--- spec/lib/john_stamos_spec.rb
+++ spec/lib/john_stamos_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe JohnStamos, :vcr do
 
   describe '.search_pins' do
-    it { should respond_to :search_pins }
+    it { should respond_to(:search_pins) }
 
     it 'returns the correct number of pins' do
       subject.search_pins('uncle jesse').should have_exactly(50).pins
@@ -12,7 +12,7 @@ describe JohnStamos, :vcr do
   end
 
   describe '.pin' do
-    it { should respond_to :pin }
+    it { should respond_to(:pin) }
 
     it 'returns a Pin' do
       subject.pin(412149803369441273).should be_a(JohnStamos::Pin)
@@ -20,7 +20,7 @@ describe JohnStamos, :vcr do
   end
 
   describe '.pinner' do
-    it { should respond_to :pinner }
+    it { should respond_to(:pinner) }
 
     it 'returns a Pinner' do
       subject.pinner('johnstamosgem').should be_a(JohnStamos::Pinner)
